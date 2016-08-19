c523ba17735e52f2aa768d90ec99733b7d14c32e
Marilena Istrate <marilena.istrate@gmail.com>
2016-08-19 12:24:27 +0300

    JS Warning

--- WikiIndexer/src/main/resources/static/js/formValidation.js
+++ WikiIndexer/src/main/resources/static/js/formValidation.js
@@ -2,8 +2,10 @@
  * Created by mistrate on 8/18/2016.
  */
 function validateForm() {
-    if (document.mainForm.articleName.value == "")
-        if (document.mainForm.fileName.value == "") {
-            return false;
+    if (document.mainForm.articleName.value == "" && document.mainForm.fileName.value == "") {
+        document.getElementById('errors').style.color = '#F0FFFF';
+        document.getElementById('errors').style.fontSize = 'large';
+        document.getElementById('errors').innerHTML="*Please enter a title or upload a file";
+        return false
         }
 }
